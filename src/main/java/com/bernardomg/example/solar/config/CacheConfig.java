fe4f1f8ddd0864a7d060da902f1c12069060530f
Bernardo <programming@bernardomg.com>
2021-01-06 19:14:47 +0100

    Changed classpath

---
+++ src/main/java/com/bernardomg/example/solar/config/CacheConfig.java
@@ -0,0 +1,15 @@
+
+package com.bernardomg.example.solar.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    public CacheConfig() {
+        super();
+    }
+
+}
