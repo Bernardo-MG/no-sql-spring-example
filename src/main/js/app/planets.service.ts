9eb1303dfe7f9eb60a93b0b13a1cb46e44a6e91c
Bernardo <programming@bernardomg.com>
2021-01-06 20:26:56 +0100

    Corrected model

--- src/main/js/app/planets.service.ts
+++ src/main/js/app/planets.service.ts
@@ -8,56 +8,56 @@ export class PlanetsService {
 
   solar: Array<Planet> = [
     {
-      name: "Mercury", data: [
+      name: "Mercury", info: [
         { id: 'tilt', value: 0.03, label: 'Tilt' },
         { id: 'radius', value: 2439.7, label: 'Radius' },
         { id: 'period', value: 58.65, label: 'Period' }
       ]
     },
     {
-      name: "Venus", data: [
+      name: "Venus", info: [
         { id: 'tilt', value: 2.64, label: 'Tilt' },
         { id: 'radius', value: 6051.8, label: 'Radius' },
         { id: 'period', value: -243, label: 'Period' }
       ]
     },
     {
-      name: "Earth", data: [
+      name: "Earth", info: [
         { id: 'tilt', value: 23.44, label: 'Tilt' },
         { id: 'radius', value: 6371, label: 'Radius' },
         { id: 'period', value: 1, label: 'Period' }
       ]
     },
     {
-      name: "Mars", data: [
+      name: "Mars", info: [
         { id: 'tilt', value: 6.68, label: 'Tilt' },
         { id: 'radius', value: 3389.5, label: 'Radius' },
         { id: 'period', value: 1.03, label: 'Period' }
       ]
     },
     {
-      name: "Jupiter", data: [
+      name: "Jupiter", info: [
         { id: 'tilt', value: 25.19, label: 'Tilt' },
         { id: 'radius', value: 69911, label: 'Radius' },
         { id: 'period', value: 0.41, label: 'Period' }
       ]
     },
     {
-      name: "Saturn", data: [
+      name: "Saturn", info: [
         { id: 'tilt', value: 26.73, label: 'Tilt' },
         { id: 'radius', value: 58232, label: 'Radius' },
         { id: 'period', value: 0.44, label: 'Period' }
       ]
     },
     {
-      name: "Uranus", data: [
+      name: "Uranus", info: [
         { id: 'tilt', value: 82.23, label: 'Tilt' },
         { id: 'radius', value: 25362, label: 'Radius' },
         { id: 'period', value: -0.72, label: 'Period' }
       ]
     },
     {
-      name: "Neptune", data: [
+      name: "Neptune", info: [
         { id: 'tilt', value: 28.32, label: 'Tilt' },
         { id: 'radius', value: 24622, label: 'Radius' },
         { id: 'period', value: 0.72, label: 'Period' }
