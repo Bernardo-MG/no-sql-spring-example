1802d2fcea47b29fb44f173e1b08a34c766c3ab8
Bernardo <programming@bernardomg.com>
2021-01-08 17:09:02 +0100

    Added test import

--- src/main/js/app/solar-system/solar-system.component.spec.ts
+++ src/main/js/app/solar-system/solar-system.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientModule } from '@angular/common/http';
 
 import { SolarSystemComponent } from './solar-system.component';
 
@@ -10,7 +11,7 @@ describe('SolarSystemComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ SolarSystemComponent ],
-      imports: [RouterTestingModule]
+      imports: [HttpClientModule, RouterTestingModule]
     })
     .compileComponents();
   }));
