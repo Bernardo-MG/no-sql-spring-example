10374578ba53a9ff6011e18b7dacbaf77d0a2c31
Bernardo <programming@bernardomg.com>
2021-01-08 16:58:46 +0100

    Changed test import

--- src/main/js/app/planets.service.spec.ts
+++ src/main/js/app/planets.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { PlanetsService } from './planets.service';
 
@@ -8,7 +8,7 @@ describe('PlanetsService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
+      imports: [HttpClientModule]
     });
     service = TestBed.inject(PlanetsService);
   });
