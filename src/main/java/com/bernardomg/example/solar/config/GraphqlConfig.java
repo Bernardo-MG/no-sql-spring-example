9095bbca418602ec12c888b4747e4e53f3397ab2
Bernardo <programming@bernardomg.com>
2021-02-07 18:58:18 +0100

    Set up GraphQL endpoint

--- src/main/java/com/bernardomg/example/solar/config/GraphqlConfig.java
+++ src/main/java/com/bernardomg/example/solar/config/GraphqlConfig.java
@@ -1,35 +1,61 @@
 
 package com.bernardomg.example.solar.config;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
-import graphql.Scalars;
-import graphql.schema.DataFetcher;
-import graphql.schema.FieldCoordinates;
-import graphql.schema.GraphQLCodeRegistry;
-import graphql.schema.GraphQLObjectType;
+import com.bernardomg.example.solar.graphql.AllPlanetsDataFetcher;
+
+import graphql.GraphQL;
 import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
 
 @Configuration
 public class GraphqlConfig {
+
+    @Autowired
+    private AllPlanetsDataFetcher allPlanetsDataFetcher;
+
+    private GraphQL               graphQL;
+
+    @Value("classpath:graphql/planets.graphqls")
+    private Resource              resource;
 
     public GraphqlConfig() {
         super();
     }
 
     @Bean
-    public GraphQLSchema schema() {
-        final DataFetcher<String> test = env -> "response";
-        return GraphQLSchema.newSchema().query(GraphQLObjectType.newObject()
-                .name("query")
-                .field(field -> field.name("test").type(Scalars.GraphQLString))
-                .build())
-                .codeRegistry(
-                        GraphQLCodeRegistry.newCodeRegistry()
-                                .dataFetcher(FieldCoordinates
-                                        .coordinates("query", "test"), test)
-                                .build())
+    public GraphQL graphQL() {
+        return graphQL;
+    }
+
+    @PostConstruct
+    public void init() throws IOException {
+        final File file = resource.getFile();
+        final TypeDefinitionRegistry typeDefinitionRegistry = new SchemaParser()
+                .parse(file);
+        final RuntimeWiring runtimeWiring = buildRuntimeWiring();
+        final GraphQLSchema graphQLSchema = new SchemaGenerator()
+                .makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+        this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+    }
+
+    private RuntimeWiring buildRuntimeWiring() {
+        return RuntimeWiring.newRuntimeWiring()
+                .type("Query", typeWiring -> typeWiring
+                        .dataFetcher("allPlanets", allPlanetsDataFetcher))
                 .build();
     }
 
---
+++ src/main/java/com/bernardomg/example/solar/controller/PlanetGraphqlController.java
@@ -0,0 +1,64 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2021 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.bernardomg.example.solar.controller;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+
+/**
+ * Rest controller for the example entities.
+ * 
+ * @author Bernardo Mart&iacute;nez Garrido
+ */
+@RestController
+@RequestMapping("/api/planet")
+public class PlanetGraphqlController {
+
+    private GraphQL graphql;
+
+    @Autowired
+    public PlanetGraphqlController(final GraphQL gql) {
+        super();
+
+        graphql = checkNotNull(gql);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> movies(@RequestBody final String query) {
+        final ExecutionResult execute = graphql.execute(query);
+        return new ResponseEntity<>(execute, HttpStatus.OK);
+    }
+
+}
