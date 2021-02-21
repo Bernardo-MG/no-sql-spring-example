6d79780cf6b0a193968dc3b9ebf01a7e3336b608
Bernardo <programming@bernardomg.com>
2021-02-21 20:52:52 +0100

    Cleaned up frontend

--- src/main/js/app/planets.service.ts
+++ src/main/js/app/planets.service.ts
@@ -6,7 +6,11 @@ import { map } from 'rxjs/operators';
 import { Apollo, gql } from 'apollo-angular';
 import { ApolloQueryResult } from '@apollo/client/core';
 
-
+/**
+ * Service handling the acquisition of planet data.
+ * 
+ * The data is requested to a GraphQL endpoint.
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -16,6 +20,11 @@ export class PlanetsService {
     private apollo: Apollo
   ) { }
 
+  /**
+   * Returns all teh existing planets.
+   * 
+   * @returns all the planets
+   */
   getPlanets(): Observable<Planet[]> {
     return this.apollo
       .watchQuery({
@@ -34,6 +43,12 @@ export class PlanetsService {
       .valueChanges.pipe(map((response: ApolloQueryResult<PlanetResponse>) => { return response.data.allPlanets }));
   }
 
+  /**
+   * Returns a single planet, for the received id.
+   * 
+   * @param id 
+   * @returns the planet for the id
+   */
   getPlanet(id: String): Observable<Planet> {
     return this.apollo
       .watchQuery({
